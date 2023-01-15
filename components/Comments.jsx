767198356376bec99625d4e2dda21ccd2627d1d2
GargDivanshu <divanshu.garg113@gmail.com>
2023-01-15 23:31:41 +0530

    error solves

---
+++ components/Comments.jsx
@@ -0,0 +1,46 @@
+import React, {useState, useEffect} from 'react'
+import moment from 'moment';
+import parse from 'html-react-parser';
+import { getComments } from './../services';
+
+const Comments = ({slug}) => {
+  const [comment, setComment] = useState([]);
+
+  useEffect(() =>{
+     getComments(slug)
+     .then((result)=>setComment(result))
+  }, [])
+  return (
+    <>
+      {comment.length > 0 && (
+        <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+        <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+        {comment.length}
+        {' '}
+        Comments
+        </h3>
+        {comment.map((comment) => (
+          <div key={comment.createdAt} className="border-b border-gray-100 mb-4 pb-4">
+            <p className="mb-4">
+               <span>
+                {comment.name}
+                </span>
+                {' '}
+                on 
+                {' '}
+                {moment(comment.createdAt).format('MMM DD, YYYY')}
+                <p className="whitespace-pre-line text-gray-600 w-full">
+                {parse(comment.comment)}
+
+                </p>
+            </p>
+          </div>
+        ))}
+
+        </div>
+      )}
+    </>
+  )
+}
+
+export default Comments
